df779426c517156515630cbae5c27f7cce4dbcff
Eduardo Marchena Rosales <edumar_111@hotmail.com>
2020-04-13 01:23:43 -0500

    add config-service

---
+++ config-service/src/test/java/academy/digitallab/store/config/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package academy.digitallab.store.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
