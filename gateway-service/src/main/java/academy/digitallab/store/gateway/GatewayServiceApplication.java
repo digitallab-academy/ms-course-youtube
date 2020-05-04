6b0347c105f2c7fc23943446be5ecdfccaf66821
Eduardo Marchena Rosales <edumar_111@hotmail.com>
2020-05-04 08:02:20 -0500

    add gateway service

---
+++ gateway-service/src/main/java/academy/digitallab/store/gateway/GatewayServiceApplication.java
@@ -0,0 +1,15 @@
+package academy.digitallab.store.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class GatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayServiceApplication.class, args);
+	}
+
+}
