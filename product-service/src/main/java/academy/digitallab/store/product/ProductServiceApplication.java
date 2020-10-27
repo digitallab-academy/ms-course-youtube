6c5aa60f27e6e3fee07e91d55998d2ad783ae884
tataco <tatan100191@gmail.com>
2020-10-26 23:19:54 -0500

    commit REST

--- product-service/src/main/java/academy/digitallab/store/product/ProductServiceApplication.java
+++ product-service/src/main/java/academy/digitallab/store/product/ProductServiceApplication.java
@@ -3,6 +3,7 @@ package academy.digitallab.store.product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class ProductServiceApplication {
 
---
+++ registry-service/src/test/java/academy/digitallab/store/registryservice/RegistryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package academy.digitallab.store.registryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
