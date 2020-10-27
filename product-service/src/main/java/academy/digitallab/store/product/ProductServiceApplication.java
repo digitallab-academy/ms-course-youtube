20191f27b1512df43cd330107baa51853393baa6
tataco <tatan100191@gmail.com>
2020-10-26 23:42:47 -0500

    changeBootstraps

--- product-service/src/main/java/academy/digitallab/store/product/ProductServiceApplication.java
+++ product-service/src/main/java/academy/digitallab/store/product/ProductServiceApplication.java
@@ -2,6 +2,7 @@ package academy.digitallab.store.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
 @SpringBootApplication
